bb99fccf33e058bf610bd0970ec41e4e6c698ba0
a.kosyrev <a.kosyrev@deltasolutions.ru>
2021-12-03 23:30:45 +0300

    feat: caching cache dir info

--- mod_utils/locate_cache_dir.ts
+++ mod_utils/locate_cache_dir.ts
@@ -1,9 +1,14 @@
+let cacheDir: string | undefined;
+
 export async function locateCacheDir(): Promise<string> {
+  if (cacheDir) {
+    return cacheDir;
+  }
   const process = await Deno.run({
     cmd: ["deno", "info", "--json"],
     stdout: "piped",
   });
   const [raw] = await Promise.all([process.output(), process.status()]);
   const { modulesCache } = JSON.parse(new TextDecoder().decode(raw));
-  return modulesCache;
+  return (cacheDir = modulesCache);
 }
