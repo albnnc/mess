6a9e5569fee1806457d591f712d2722be6d59d68
a.kosyrev <a.kosyrev@deltasolutions.ru>
2022-01-18 00:56:16 +0300

    fix: memo hook

--- cobalt/hooks/use_memo.ts
+++ cobalt/hooks/use_memo.ts
@@ -3,6 +3,10 @@ import { compareDeps } from "../utils/mod.ts";
 import { useState } from "./use_state.ts";
 
 export const useMemo = <T>(fn: () => T, deps: Deps): T => {
-  const [data] = useState({ value: fn(), deps });
-  return compareDeps(deps, data.deps) ? data.value : (data.value = fn());
+  const [data] = useState(() => ({ value: fn(), deps }));
+  if (!compareDeps(deps, data.deps)) {
+    data.value = fn();
+    data.deps = deps;
+  }
+  return data.value;
 };
--- cobalt/utils/compare_deps.ts
+++ cobalt/utils/compare_deps.ts
@@ -3,6 +3,6 @@ export function compareDeps(a: unknown, b: unknown) {
     Array.isArray(a) &&
     Array.isArray(b) &&
     a.length === b.length &&
-    a.every((v, i) => v === b?.[i])
+    a.every((v, i) => v === b[i])
   );
 }
