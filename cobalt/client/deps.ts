7dfc7b83821b52d62c2837eb03c9b7abf3352015
a.kosyrev <a.kosyrev@deltasolutions.ru>
2022-03-07 00:28:56 +0300

    chore: add comment

--- cobalt/client/deps.ts
+++ cobalt/client/deps.ts
@@ -1,6 +1,8 @@
 import "../../tene/register.ts";
 
+// This import is huge, it costs us 45kb. Consider removing.
 export * as urlPattern from "https://cdn.skypack.dev/urlpattern-polyfill@1.0.0-rc5?dts";
+
 export * from "../../rembrandt/mod.ts";
 export * from "../../palet/mod.ts";
 export * from "../../tene/mod.ts";
