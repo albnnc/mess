e5cb1ad1d2fb9add48eb00ac28fb41763d775c15
a.kosyrev <a.kosyrev@deltasolutions.ru>
2022-03-12 04:30:07 +0300

    fix: make element prop changes instant

--- rembrandt/hooks/use_prop.ts
+++ rembrandt/hooks/use_prop.ts
@@ -1,20 +1,12 @@
-import { Initializer, CustomElement } from "../types.ts";
+import { Initializer, CustomElement, StateUpdate } from "../types.ts";
 import { useElement } from "./use_element.ts";
 import { useState } from "./use_state.ts";
-import { useEffect } from "./use_effect.ts";
 import { getInitialValue } from "../utils/mod.ts";
+import { useMemoFn } from "./use_memo_fn.ts";
 
 export const useProp = <T>(name: string, initializer: Initializer<T>) => {
   const element = useElement();
-  // TODO: Consider defining getter / setter pair bofore
-  // one is able to set properties (e.g. in element constructor)
-  // in order to remove this check.
-  const [value, setValue] = useState(() =>
-    name in element
-      ? (element as CustomElement<Record<string, T>>)[name]
-      : getInitialValue(initializer)
-  );
-  useEffect(() => {
+  const defineHandlers = useMemoFn((value: T) => {
     Object.defineProperty(element, name, {
       configurable: true,
       enumerable: true,
@@ -22,9 +14,27 @@ export const useProp = <T>(name: string, initializer: Initializer<T>) => {
         return value;
       },
       set(v: T) {
-        setValue(() => v);
+        setProp(() => v);
       },
     });
-  }, [value]);
-  return [value, setValue] as const;
+  }, []);
+  const setProp = useMemoFn((update: StateUpdate<T>) => {
+    setValue((prior) => {
+      const next = update instanceof Function ? update(prior) : update;
+      defineHandlers(next);
+      return next;
+    });
+  }, []);
+  const [value, setValue] = useState(() => {
+    const initialValue =
+      // TODO: Consider defining getter / setter pair bofore
+      // one is able to set properties (e.g. in element constructor)
+      // in order to remove this check.
+      name in element
+        ? (element as CustomElement<Record<string, T>>)[name]
+        : getInitialValue(initializer);
+    defineHandlers(initialValue);
+    return initialValue;
+  });
+  return [value, setProp] as const;
 };
