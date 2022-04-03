309243a892c42d705d6ce2f07fab6f9d5319f3d8
a.kosyrev <a.kosyrev@deltasolutions.ru>
2022-04-04 00:24:20 +0300

    fix: add missing exports

---
+++ eventually/handlers/mod.ts
@@ -0,0 +1,2 @@
+export * from "./handle_creation.ts";
+export * from "./handle_mutation.ts";
--- eventually/mod.ts
+++ eventually/mod.ts
@@ -1,23 +1,2 @@
-import { collections, nats } from "./deps.ts";
-
-const nc = await nats.connect({
-  servers: "localhost:4222",
-});
-
-const jsm = await nc.jetstreamManager();
-const streamName = "SAMPLE";
-const subjects = ["SAMPLE.A"];
-
-try {
-  const { config } = await jsm.streams.info(streamName);
-  const unionSubjects = collections.union(config.subjects, subjects);
-  if (config.subjects.length !== unionSubjects.length) {
-    await jsm.streams.update(streamName, { subjects: unionSubjects });
-  }
-} catch {
-  await jsm.streams.add({ name: streamName, subjects });
-}
-
-const info = await jsm.streams.info(streamName);
-
-await nc.close();
+export * from "./handlers/mod.ts";
+export * from "./utils/mod.ts";
