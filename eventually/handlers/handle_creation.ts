7e365c4307a2c2391f4edbbf13387427e897097b
a.kosyrev <a.kosyrev@deltasolutions.ru>
2022-04-05 17:27:38 +0300

    feat: mongo config

--- eventually/handlers/handle_creation.ts
+++ eventually/handlers/handle_creation.ts
@@ -1,11 +1,12 @@
-import { scheming } from "../deps.ts";
+import { mongo, scheming } from "../deps.ts";
 import { handleMutation, MutationOptions } from "./handle_mutation.ts";
 
 export interface CreationOptions extends Omit<MutationOptions, "process"> {
+  db: mongo.Database;
   schema: Record<string, unknown>;
 }
 
-export async function handleCreation({ schema, ...rest }: CreationOptions) {
+export async function handleCreation({ db, schema, ...rest }: CreationOptions) {
   const validate = scheming.createValidator(schema, { mode: "w" });
   await handleMutation({
     process: (data) => {
