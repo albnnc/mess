be608a108379c5e52570fb0ea101b274cc4df7db
a.kosyrev <a.kosyrev@deltasolutions.ru>
2022-04-12 14:01:38 +0300

    refactor: better response handling

--- eventually/utils/validate_response.ts
+++ eventually/utils/validate_response.ts
@@ -1,9 +1,10 @@
 import { nats } from "../deps.ts";
+import { StatusCodeError } from "../errors/mod.ts";
 
 export function validateResponse(msg: nats.Msg) {
   const statusCode = msg.headers?.get("Status-Code");
   const description = msg.headers?.get("Description");
   if (statusCode && !statusCode.startsWith("2")) {
-    throw new Error(description);
+    throw new StatusCodeError(statusCode, description);
   }
 }
